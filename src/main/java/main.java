e4965184b017c09d9f55871bf2bba6f349439868
conroyr41 <conroyr41@gmail.com>
2021-09-02 12:24:33 -0400

    first commit

---
+++ src/main/java/main.java
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Conroy Ricketts
+ */
+
+import java.util.Scanner;
+
+public class main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Enter an input string.");
+
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+
+        System.out.println("The string that you entered was \"" + s + "\" which has " + s.length() + " characters!");
+    }
+}
